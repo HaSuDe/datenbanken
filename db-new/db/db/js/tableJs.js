47641f4cb9b05783f3b38452492260818c016629
Cedric <dcedric@stud.hs-bremen.de>
2014-06-03 10:12:38 +0200

    DB New eingepflegt

--- db-new/db/db/js/tableJs.js
+++
@@ -1,7 +0,0 @@
-$( "#addRowBtn" ).on( "click", function() {
-
-	$('#mainTable > tbody:last').append('<tr><td>Your Article</td><td>Amount</td><td></td><td></td></tr>');
-	// Nach erzeugen muss die Methode noch einmal ausgeführt werden
-	$('#mainTable').editableTableWidget().numericInputExample().find('td:first').focus();
-	$('#textAreaEditor').editableTableWidget({editor: $('<textarea>')});
-});
