ef79a79e8c8b04771e573d591488a2cdc1ef6858
nhatje <nhatje@gmx.de>
2014-06-23 13:00:50 +0200

    LoginJs gelöscht, index in php verwandelt

--- js/login.js
+++
@@ -1,6 +0,0 @@
-$( '#loginBtn' ).on( 'click', function() {
-	// TODO Prüfen der eingaben
-	//$("#main-content").load( "profile.html");
-	//window.open("profile.html");
-	window.location.href = "12345";
-});
